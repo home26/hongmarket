151ffeff9bb6cfa946cf26cb547af906a6355e69
Hong Li <lih224@mcmaster.ca>
2020-12-07 11:45:41 -0500

    20201207

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -34,7 +34,12 @@
                       <div class="children"></div>
                   </div>
               </div>
-              <div class="header-search"></div>
+              <div class="header-search">
+                  <div class="wrapper">
+                      <input type="text" name="keyword">
+                      <a href="javascript:;"></a>
+                  </div>
+              </div>
           </div>
       </div>
     </div>
@@ -98,6 +103,7 @@ export default {
                             height: 55px;
                             background: url('/imgs/hm-logo.png') no-repeat center;
                             background-size: 55px;
+                            //@include bgImg(55px,55px,'/imgs/hm-logo.png',55px);
                             transition: margin .2s;
                         }
                         &:after{
@@ -107,6 +113,7 @@ export default {
                             height: 55px;
                             background: url('/imgs/hm-home.png') no-repeat center;
                             background-size: 55px;
+                            //@include bgImg(55px,55px,'/imgs/hm-logo.png',55px);
                         }
                         &:hover:before{
                             margin-left: -55px;
@@ -116,7 +123,7 @@ export default {
                 }
                 .header-menu{
                     display: inline-block;
-                    width:200px;
+                    width:643px;
                     padding-left: 209px;
                     .item-menu{
                         display: inline-block;
@@ -124,11 +131,37 @@ export default {
                         font-weight: bold;
                         font-size: 16px;
                         line-height: 112px;
+                        margin-right: 20px;
                         span{
                             cursor: pointer;
                         }
                         &:hover{
                             
+                        }
+                    }
+                }
+                .header-search{
+                    width:319px;
+                    .wrapper{
+                        height:50px;
+                        border:1px solid #E0E0E0;
+                        display: flex;
+                        align-items: center;
+                        input{
+                            border: none;
+                            box-sizing: border-box;
+                            border-right: 1px solid #E0E0E0;
+                            width:264px;
+                            height:50px;
+                            padding-left: 14px;
+                        }
+                        a{
+                            display: inline-block;
+                            width:18px;
+                            height:18px;
+                            background: url('/imgs/icon-search.png') no-repeat center;
+                            background-size: contain;
+                            margin-left: 17px;
                         }
                     }
                 }
