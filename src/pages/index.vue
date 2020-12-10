67f456d0d516dcdf9a551a0de38eb1d8bdfa6d2d
Hong Li <lih224@mcmaster.ca>
2020-12-09 23:35:51 -0500

    20201209

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -51,12 +51,12 @@
             </div>
             <div class="ads-box">
                 <a v-bind:href="'/#/product/'+item.id" v-for="(item, index) in adsList" v-bind:key="index">
-                    <img v-bind:src="item.img" alt="">
+                    <img v-lazy="item.img" alt="">
                 </a>
             </div>
             <div class="banner">
                 <a href="/#/product/30">
-                    <img src="/imgs/banner-1.png" alt="">
+                    <img v-lazy="'/imgs/banner-1.png'" alt="">
                 </a>
             </div>
         </div>
@@ -65,14 +65,14 @@
                     <h2>Phone</h2>
                     <div class="wrapper">
                     <div class="banner-left">
-                        <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+                        <a href="/#/product/35"><img v-lazy="'/imgs/mix-alpha.jpg'" alt=""></a>
                     </div>
                     <div class="list-box">
                         <div class="list" v-for="(arr, i) in phoneList" v-bind:key="i">
                             <div class="item" v-for="(item, j) in arr" v-bind:key="j">
                                 <span v-bind:class="{'new-pro':j%2==0}">NEW!</span>
                                 <div class="item-img">
-                                    <img v-bind:src="item.mainImage" alt="">
+                                    <img v-lazy="item.mainImage" alt="">
                                 </div>
                                 <div class="item-info">
                                     <h3>{{item.name}}</h3>
@@ -210,7 +210,7 @@
                    this.showModal = true;
                })*/
             },
-            gotoCart(){
+            goToCart(){
                 this.$router.push('/cart');
             }
         }
