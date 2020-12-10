2492c31bb75c1441375ccbceabb1864aecfff5c6
Hong Li <lih224@mcmaster.ca>
2020-12-10 16:35:52 -0500

    20201210

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import mutations from './mutations'
 import actions from './action'
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const state = {
     username:'', // login username
     cartCount:0 // number of products in the cart
-}
+};
 export default new Vue.Store({
     state,
     mutations,
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,5 +2,5 @@
  * Vuex-mutations
  */
 export default {
-    
+       
 }
