1668f263890f3a0c03fbc51fe56a29a871021c71
Hong Li <lih224@mcmaster.ca>
2020-12-11 14:10:38 -0500

    20201211

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,8 +1,25 @@
 <template>
-    <div>order-header</div>
+    <div class="order-header">
+        <div class="container">
+            <div class="header-logo">
+                <a href="/#/index"></a>
+            </div>
+            <div class="title">
+                <h2>My Cart<span></span></h2>
+            </div>
+            <div class="username">
+                <a href="javascript:;">Jack</a>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 export default {
     name:'order-header'
 }
 </script>
+<style lang="scss">
+    .order-header{
+        padding: 30px 0;  
+    }
+</style>
