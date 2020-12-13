38ba5bbdc031cb0b4374807f83a5d776493c26ac
home26 <jeffery3206@gmail.com>
2020-12-13 11:15:37 -0500

    20201213

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>order
-        <order-header></order-header>
+    <div>
+        <order-header v-bind:title="title">
+            <template v-slot:tip>
+                <span>{{tip}}</span>
+            </template>
+        </order-header>
         <router-view></router-view>
+        <service-bar></service-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
 import OrderHeader from '../components/OrderHeader.vue'
+import ServiceBar from './../components/ServiceBar'
 import NavFooter from '../components/NavFooter.vue'
 export default {
-  components: { OrderHeader, NavFooter},
-    name:'order'
+  name:'order',
+  data(){
+      return{
+          title:'',
+
+      }
+  },
+  components: { OrderHeader, ServiceBar, NavFooter},
+  mounted(){
+      let path = this.$route.path;
+      if(path == '/order/confirm'){
+          this.title = 'Confirm Order';
+          this.tip = 'Wish you a happy shopping journey!';
+      }else if(path == '/order/list'){
+          this.title = 'Order List';
+          this.tip = 'Wish you a happy shopping journey!';
+      }else if(path == '/order/pay'){
+          this.title = 'Pay Order';
+          this.tip = 'Wish you a happy shopping journey!';
+      }
+  }
+
 }
 </script>
